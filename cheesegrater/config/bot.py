4942c799b0499be9b1d3610d65222e4c055f1744
tommygithub1 <64607897+tommygithub1@users.noreply.github.com>
2021-01-27 20:37:09 +0000

    pog

--- cheesegrater/config/bot.py
+++ cheesegrater/config/bot.py
@@ -1,17 +1,23 @@
 # discord.py
 import os
-
 import discord
 
 with open('token.txt', 'r') as file:
     TOKEN = file.read().replace('\n', '')
+
 with open('server.txt', 'r') as file:
-    SERVERNAME = file.read().replace('')
+    SERVERNAME = file.read().replace('\n', '')
 
 client = discord.Client()
 
 @client.event
 async def on_ready():
-    print(f'{client.user} has connected to Discord!')
+    for guild in client.guilds:
+        if guild.name == SERVERNAME:
+            break
+    print(
+        f'{client.user} is connected to the following server:\n'
+        f'{guild.name}( id: {guild.id})'
+    )
 
 client.run(TOKEN)
