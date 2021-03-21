09e9eadaa127c9e546c8b589d6ae9adf3999ab82
tommygithub1 <64607897+tommygithub1@users.noreply.github.com>
2021-03-21 19:22:16 +0000

    .env go bye bye
    remove .env :yikes:

--- cheesegrater/config/bot.py
+++ cheesegrater/config/bot.py
@@ -1,23 +1,39 @@
-# discord.py
+# .py
 import os
 import discord
-
+import random
+cmdpre = "//"
 with open('token.txt', 'r') as file:
     TOKEN = file.read().replace('\n', '')
-
 with open('server.txt', 'r') as file:
-    SERVERNAME = file.read().replace('\n', '')
+    GUILD = file.read().replace('\n', '')
 
 client = discord.Client()
 
 @client.event
 async def on_ready():
-    for guild in client.guilds:
-        if guild.name == SERVERNAME:
-            break
+    guild = discord.utils.get(client.guilds, name=GUILD)
     print(
-        f'{client.user} is connected to the following server:\n'
+        f'{client.user} is connected to the following guild:\n'
         f'{guild.name}( id: {guild.id})'
     )
 
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    pog_quotes = [
+        'Pog',
+        'POGGERS',
+        'ME WHEN POG :O',
+        'POGCHAMP',
+    ]
+
+    if message.content == '//test':
+        response = random.choice(pog_quotes)
+        await message.channel.send(response)
+        
 client.run(TOKEN)
+
+
