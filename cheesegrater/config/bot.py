16341f1342947e0b02f8211ddb6848abcdfab79a
tommygithub1 <64607897+tommygithub1@users.noreply.github.com>
2021-03-22 14:21:02 +0000

    reset the bot, to make it good and pogger

--- cheesegrater/config/bot.py
+++ cheesegrater/config/bot.py
@@ -1,39 +1,20 @@
-# .py
-import os
 import discord
-import random
-cmdpre = "//"
-with open('token.txt', 'r') as file:
-    TOKEN = file.read().replace('\n', '')
-with open('server.txt', 'r') as file:
-    GUILD = file.read().replace('\n', '')
+import os
 
 client = discord.Client()
 
 @client.event
 async def on_ready():
-    guild = discord.utils.get(client.guilds, name=GUILD)
-    print(
-        f'{client.user} is connected to the following guild:\n'
-        f'{guild.name}( id: {guild.id})'
-    )
+    print('We have logged in as {0.user}'.format(client))
 
 @client.event
 async def on_message(message):
     if message.author == client.user:
         return
 
-    pog_quotes = [
-        'Pog',
-        'POGGERS',
-        'ME WHEN POG :O',
-        'POGCHAMP',
-    ]
+    if message.content.startswith('$hello'):
+        await message.channel.send('Hello!')
 
-    if message.content == '//test':
-        response = random.choice(pog_quotes)
-        await message.channel.send(response)
-        
-client.run(TOKEN)
+client.run(os.getenv('TOKEN'))
 
 
