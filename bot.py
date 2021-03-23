dffb3b734276139e34b5534ddb649baf511bac54
tommygithub1 <64607897+tommygithub1@users.noreply.github.com>
2021-03-23 11:26:25 +0000

    Added start output and status

--- bot.py
+++ bot.py
@@ -3,12 +3,34 @@ import os
 import sys
 from discord.ext import commands
 from discord.ext.commands.core import command    
+from discord.ext.commands import *
 from dotenv import load_dotenv
+import bcolors
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKCYAN = '\033[96m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+    ITALIC = '\033[3m'
 load_dotenv()
 trusted = ["674332158973706261"]
 
 
 bot = commands.Bot(command_prefix=(os.getenv("PREFIX")))
+
+@bot.event
+async def on_ready():
+    print(f'''{bcolors.BOLD + bcolors.OKBLUE}Connected successfully!
+Logged in as {bcolors.OKCYAN}{bot.user.name}{bcolors.OKBLUE}, with the ID {bcolors.OKCYAN}{bot.user.id}{bcolors.ENDC}''')
+    status = "you grate some cheese 😳"
+    await bot.change_presence(activity=discord.Activity(name=status, type=discord.ActivityType.watching))
+    print(f'{bcolors.BOLD + bcolors.OKBLUE}Status set to "{bcolors.OKCYAN}watching {status}{bcolors.OKBLUE}"{bcolors.ENDC}')
+    print(f"{bcolors.BOLD + bcolors.OKBLUE}------------------------------------------------------{bcolors.ENDC}")
 
 @bot.command(help="returns the arguments")
 async def echo(ctx, *, arg):
