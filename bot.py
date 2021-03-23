8c1a90630e2da985cedcebcedfcc88226b8db2a4
tommygithub1 <64607897+tommygithub1@users.noreply.github.com>
2021-03-23 11:11:20 +0000

    Added a ping command, and the echo command only works with me as the user now

--- bot.py
+++ bot.py
@@ -1,16 +1,38 @@
 import discord
 import os
+import sys
 from discord.ext import commands
 from discord.ext.commands.core import command    
 from dotenv import load_dotenv
 load_dotenv()
+trusted = ["674332158973706261"]
+
 
 bot = commands.Bot(command_prefix=(os.getenv("PREFIX")))
 
-@bot.command()
-async def test(ctx, arg):
-    await ctx.send(arg)
+@bot.command(help="returns the arguments")
+async def echo(ctx, *, arg):
+    id =  ctx.author.id
+    if id == 674332158973706261:
+        await ctx.send(arg)
+    else:
+        await ctx.message.add_reaction("🔐")
+
+@bot.command(help="Gets the ping of the bot and returns it in an embed")
+async def ping(ctx):
+    await ctx.message.add_reaction("🏓")
+    botPing = round(bot.latency * 1000)
+    if botPing < 130:
+        color = 0x33D23F
+    elif botPing > 130 and botPing < 180:
+        color = 0xff8c00
+    else: 
+        color = 0xff2200
+    embed = discord.Embed(title="Pong! :ping_pong:", description=f"The ping is **{botPing}ms!**")
+    await ctx.reply(embed=embed)
 
 
 
-bot.run(os.getenv('TOKEN'))
+
+
+bot.run(os.getenv('TKN'))
